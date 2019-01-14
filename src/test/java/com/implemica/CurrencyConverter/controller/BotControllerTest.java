336e97e9eaa3900580e4b552bb0b381d035e5476
luchchaya <vbienrf28>
2019-01-14 14:27:45 +0200

    Changed BotControllerTest class from jUnit4 to jUnit5

--- src/test/java/com/implemica/CurrencyConverter/controller/BotControllerTest.java
+++ src/test/java/com/implemica/CurrencyConverter/controller/BotControllerTest.java
@@ -2,28 +2,26 @@ package com.implemica.CurrencyConverter.controller;
 
 import com.implemica.CurrencyConverter.model.User;
 import com.implemica.CurrencyConverter.service.BotService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * This class tests BotController class
+ *
  * @author Daria S.
  * @version 11.01.2019 17:48
  */
 @ExtendWith(MockitoExtension.class)
-public class BotControllerTest {
+class BotControllerTest {
 
    /**
     * Bot name in Telegram.
@@ -34,168 +32,227 @@ public class BotControllerTest {
     */
    private static final String BOT_TOKEN = "736932538:AAFW981ptLJ4g1lbsVTn7HebaojMKLClEDg";
 
+   /**
+    * Start command
+    */
+   private static final String message1Text = "/start";
+   /**
+    * Stop command
+    */
+   private static final String message2Text = "/stop";
+   /**
+    * Convert command
+    */
+   private static final String message3Text = "/convert";
+   /**
+    * Some non-command word
+    */
+   private static final String message4Text = "hello";
+   /**
+    * Bot's response for start command
+    */
+   private static final String startMessage = "Hello, could I help you?";
+   /**
+    * Bot's response for stop command
+    */
+   private static final String stopMessage = "OK. You can use /convert to make me new " +
+           "convert currencies";
+   /**
+    * Bot's response for convert command
+    */
+   private static final String firstConvertMessage = "Please, type in the currency to convert from" +
+           " (example: USD)";
+   /**
+    * Bot's response for non-command word
+    */
+   private static final String wrongMessage = "Sorry, but I don't understand what \"hello\"" +
+           " means. You can use /convert to make me new convert currencies";
+   /**
+    * Bot's response for message, which contains incorrect content
+    */
+   private static final String wrongContentMessage = "Sorry, but this message contains " +
+           "incorrect content. Please, don't send me messages, which I can't handle. You can use /convert to make me new " +
+           "convert currencies";
 
+   /**
+    * A dummy implementation for BotService class, represents bot's logic
+    */
    @Mock
    private BotService botService;
 
+   /**
+    * A dummy implementation for Update class, first incoming update from telegram user
+    */
    @Mock
-   private Update update1;
-   @Mock
-   private Update update2;
-   @Mock
-   private Update update3;
-   @Mock
-   private Update update4;
-   @Mock
-   private Update update5;
+   private Update update;
 
-   @Mock
-   private Message message1;
-   @Mock
-   private Message message2;
-   @Mock
-   private Message message3;
-   @Mock
-   private Message message4;
-   @Mock
-   private Message message5;
 
+   /**
+    * A dummy implementation for Message class, first message from telegram user
+    */
    @Mock
-   private org.telegram.telegrambots.meta.api.objects.User botUser1;
-   @Mock
-   private org.telegram.telegrambots.meta.api.objects.User botUser2;
-   @Mock
-   private org.telegram.telegrambots.meta.api.objects.User botUser3;
-   @Mock
-   private org.telegram.telegrambots.meta.api.objects.User botUser4;
+   private Message message;
 
-   @Mock
-   private User user1;
-   @Mock
-   private User user2;
-   @Mock
-   private User user3;
-   @Mock
-   private User user4;
 
+   /**
+    * A dummy implementation for Telegram User class
+    */
+   @Mock
+   private org.telegram.telegrambots.meta.api.objects.User botUser;
+
+
+   /**
+    * BotController object, in which mocked object's have to be injected
+    */
    @InjectMocks
-   private static BotController controller;
+   private BotController controller;
 
-
-   private static String message1Text = "/start";
-   private static String message2Text = "/stop";
-   private static String message3Text = "/convert";
-   private static String message4Text = "hello";
-
-
+   /**
+    * Creates new BotController, which depends on mocked botService
+    */
    @BeforeEach
    void setUp() {
       controller = new BotController(botService);
-      MockitoAnnotations.initMocks(this);
-
-      when(update1.getMessage()).thenReturn(message1);
-      when(update2.getMessage()).thenReturn(message2);
-      when(update3.getMessage()).thenReturn(message3);
-      when(update4.getMessage()).thenReturn(message4);
-      when(update5.getMessage()).thenReturn(message5);
-
-      when(message1.hasText()).thenReturn(true);
-      when(message2.hasText()).thenReturn(true);
-      when(message3.hasText()).thenReturn(true);
-      when(message4.hasText()).thenReturn(true);
-      when(message5.hasText()).thenReturn(false);
-
-      when(message1.getText()).thenReturn(message1Text);
-      when(message2.getText()).thenReturn(message2Text);
-      when(message3.getText()).thenReturn(message3Text);
-      when(message4.getText()).thenReturn(message4Text);
-
-
-      when(message1.getFrom()).thenReturn(botUser1);
-      when(message2.getFrom()).thenReturn(botUser2);
-      when(message3.getFrom()).thenReturn(botUser3);
-      when(message4.getFrom()).thenReturn(botUser4);
-      when(message5.getFrom()).thenReturn(botUser1);
-
-
-      when(botUser1.getId()).thenReturn(145);
-      when(botUser1.getFirstName()).thenReturn("ludvig");
-      when(botUser1.getLastName()).thenReturn("fourteenth");
-      when(botUser1.getUserName()).thenReturn("fox");
-
-      when(botUser2.getId()).thenReturn(1289);
-      when(botUser2.getFirstName()).thenReturn("julia");
-      when(botUser2.getLastName()).thenReturn(null);
-      when(botUser2.getUserName()).thenReturn("jD");
-
-      when(botUser3.getId()).thenReturn(11111);
-      when(botUser3.getFirstName()).thenReturn("semen");
-      when(botUser3.getLastName()).thenReturn("semen");
-      when(botUser3.getUserName()).thenReturn(null);
-
-      when(botUser4.getId()).thenReturn(74);
-      when(botUser4.getFirstName()).thenReturn("mike");
-      when(botUser4.getLastName()).thenReturn(null);
-      when(botUser4.getUserName()).thenReturn(null);
-
-      user1 = controller.getInformationAboutUser(message1);
-      user2 = controller.getInformationAboutUser(message2);
-      user3 = controller.getInformationAboutUser(message3);
-      user4 = controller.getInformationAboutUser(message4);
-
-      when(botService.onUpdateReceived(message1Text, user1)).thenReturn("Hello, could I help you?");
-      when(botService.onUpdateReceived(message2Text, user2)).thenReturn("OK. You can use /convert to make me new " +
-              "convert currencies");
-      when(botService.onUpdateReceived(message3Text, user3)).thenReturn("Please, type in the currency to convert from" +
-              " (example: USD)");
-      when(botService.onUpdateReceived(message4Text, user4)).thenReturn("Sorry, but I don't understand what \"hello\"" +
-              " means. You can use /convert to make me new convert currencies");
-      when(botService.onUpdateReceived(BotService.UNIQUE, user1)).thenReturn("Sorry, but this message contains " +
-              "incorrect content. Please, don't send me messages, which I can't handle. You can use /convert to make me new " +
-              "convert currencies");
-
 
    }
 
+   /**
+    * Tests, that methods returns bots username and token correctly
+    */
    @Test
-   public void test() {
+   void test() {
       assertEquals(BOT_NAME, controller.getBotUsername());
       assertEquals(BOT_TOKEN, controller.getBotToken());
    }
 
+   /**
+    * Tests, that if user has all information about himself and his message contains specified text, and if method
+    * {@link BotController#onUpdateReceived(Update)} is called, then methods {@link Update#getMessage()} and
+    * {@link BotService#onUpdateReceived(String, User)} is called with specified parameters
+    */
    @Test
-   public void onUpdateReceivedTest() {
+   void onUpdateReceivedTest1() {
+      //update1 from user1
+      messageBehavior(message1Text, true);
+
       //user with all information
-      controller.onUpdateReceived(update1);
 
-      verify(update1).getMessage();
-      verify(botService).onUpdateReceived(message1Text, user1);
+      createUser(145, "ludvig", "fourteenth", "fox");
 
+      verifyController(message1Text, startMessage);
+   }
+
+   /**
+    * Tests, that if information about user doesn't contain his last name and his message contains specified text,
+    * and if method {@link BotController#onUpdateReceived(Update)} is called, then methods {@link Update#getMessage()}
+    * and {@link BotService#onUpdateReceived(String, User)} is called with specified parameters
+    */
+   @Test
+   void onUpdateReceivedTest2() {
+      //update2 from user2
+      messageBehavior(message2Text, true);
 
       //user without last name
-      controller.onUpdateReceived(update2);
+      createUser(1289, "Laban", null, "fox");
 
-      verify(update2).getMessage();
-      verify(botService).onUpdateReceived(message2Text, user2);
+      verifyController(message2Text, stopMessage);
+   }
+
+   /**
+    * Tests, that if information about user doesn't contain his username name and his message contains specified text,
+    * and if method {@link BotController#onUpdateReceived(Update)} is called, then methods {@link Update#getMessage()}
+    * and {@link BotService#onUpdateReceived(String, User)} is called with specified parameters
+    */
+   @Test
+   void onUpdateReceivedTest3() {
+      //update3 from user3
+
+      messageBehavior(message3Text, true);
 
       //user without username
-      controller.onUpdateReceived(update3);
+      createUser(1111, "dad", "Larsson", null);
 
-      verify(update3).getMessage();
-      verify(botService).onUpdateReceived(message3Text, user3);
+      verifyController(message3Text, firstConvertMessage);
+   }
+
+   /**
+    * Tests, that if information about user doesn't contain his last name and username, and his message contains specified text,
+    * and if method {@link BotController#onUpdateReceived(Update)} is called, then methods {@link Update#getMessage()}
+    * and {@link BotService#onUpdateReceived(String, User)} is called with specified parameters
+    */
+   @Test
+   void onUpdateReceivedTest4() {
+      //update4 from user4
+
+      messageBehavior(message4Text, true);
 
       //user without last name and username
-      controller.onUpdateReceived(update4);
+      createUser(74, "squirrel", null, null);
 
-      verify(update4).getMessage();
-      verify(botService).onUpdateReceived(message4Text, user4);
+      verifyController(message4Text, wrongMessage);
+   }
 
+   /**
+    * Tests, that if user has all information about himself and his message doesn't contains text, and if method
+    * {@link BotController#onUpdateReceived(Update)} is called, then methods {@link Update#getMessage()} and
+    * {@link BotService#onUpdateReceived(String, User)} is called with specified parameters
+    */
+   @Test
+   void onUpdateReceivedTest5() {
 
-      //non-text message
-      controller.onUpdateReceived(update5);
+      //update5 from user1
+      messageBehavior(null, false);
 
-      verify(update5).getMessage();
-      verify(botService).onUpdateReceived(BotService.UNIQUE, user1);
+      createUser(38210, "tutta", "carlson", "chicken");
+      verifyController(BotService.UNIQUE, wrongContentMessage);
+   }
+
+   /**
+    * Defines the return value when update and message methods of the mocked objects are been called
+    *
+    * @param messageText text of user's message
+    * @param hasText     true, if message has correct content (text), false - otherwise
+    */
+   private void messageBehavior(String messageText, boolean hasText) {
+      when(update.getMessage()).thenReturn(message);
+      when(message.hasText()).thenReturn(hasText);
+      if (hasText) {
+         when(message.getText()).thenReturn(messageText);
+      }
+      when(message.getFrom()).thenReturn(botUser);
+   }
+
+   /**
+    * Defines the return value when Telegram User methods of the mocked object is been called
+    *
+    * @param id       users's id
+    * @param name     user's name
+    * @param lastName user's last name
+    * @param userName user's username
+    */
+   private void createUser(int id, String name, String lastName, String userName) {
+      when(botUser.getId()).thenReturn(id);
+      when(botUser.getFirstName()).thenReturn(name);
+      when(botUser.getLastName()).thenReturn(lastName);
+      when(botUser.getUserName()).thenReturn(userName);
+   }
+
+   /**
+    * Creates User. Defines the return value when onUpdateReceived method of the mocked object botService is been called.
+    * Calls {@link BotController#onUpdateReceived(Update)} method and verifies, that methods {@link Update#getMessage()}
+    * and {@link BotService#onUpdateReceived(String, User)} were called with specified parameters.
+    *
+    * @param messageText  message from User
+    * @param responseText response to User
+    */
+   private void verifyController(String messageText, String responseText) {
+      User user = controller.getInformationAboutUser(message);
+      doReturn(responseText).when(botService).onUpdateReceived(messageText, user);
+
+      controller.onUpdateReceived(update);
+
+      verify(update).getMessage();
+      verify(botService).onUpdateReceived(messageText, user);
    }
 
 }
